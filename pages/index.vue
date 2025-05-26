255e51a6379c7a92cd94a12ed1868115bf917708
Lucas Silva <lucasvida2005@gmail.com>
2025-05-26 10:58:15 -0300

    adding an address search function

--- pages/index.vue
+++ pages/index.vue
@@ -5,16 +5,27 @@
         <!-- v-model faz com que o valor digitado será automaticamente salvo na variável 'cep' -->
         <input v-model="cep" type="number" placeholder="Digite o CEP" />
 
-        <button @click="">Buscar</button>
+        <button @click="buscarEndereco">Buscar</button>
 
         <!-- v-if exibe a div somente se a variável 'endereco' tiver algum valor (não for null ou undefined) -->
         <div v-if="endereco">
-            <p>Logradouro: {{}}</p>
-            <p>Bairro: {{}}</p>
-            <p>Cidade: {{}}</p>
-            <p>Estado: {{}}</p>
+            <p>Logradouro: {{ endereco.logradouro }}</p>
+            <p>Bairro: {{ endereco.bairro }}</p>
+            <p>Cidade: {{ endereco.localidade }}</p>
+            <p>Estado {{ endereco.estado }}</p>
         </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+// ref faz um objeto reativo, permitindo que o Vue "observe" esse valor e reaja automaticamente a mudanças
+const cep = ref("");
+const endereco = ref(null);
+
+async function buscarEndereco() {
+    const response = await $fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+    endereco.value = response;
+}
+</script>
